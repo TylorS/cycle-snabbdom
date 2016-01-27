55a2a1d6c88e798ee43d77f1e4be8869a9f6167f
TylorS <tlsteinberger167@gmail.com>
2016-01-27 17:27:08 -0500

    fix(transposition): fix `null` value children
    switch around the if statements and fix filter()
    to remove null valued children from the vTree
    
    Close #9

--- src/transposition.js
+++ src/transposition.js
@@ -1,16 +1,16 @@
 import Rx from 'rx'
 
 function transposeVTree(vTree) {
-  if (typeof vTree.data === `object` && vTree.data.static) {
-    return Rx.Observable.just(vTree)
-  } else if (!vTree) {
+  if (!vTree) {
     return null
+  } else if (typeof vTree.data === `object` && vTree.data.static) {
+    return Rx.Observable.just(vTree)
   } else if (typeof vTree.subscribe === `function`) {
     return vTree.flatMapLatest(transposeVTree)
   } else if (typeof vTree === `object`) {
     if (vTree.children && vTree.children.length > 0) {
       return Rx.Observable.combineLatest(
-        vTree.children.map(transposeVTree).filter(x => !!x),
+        vTree.children.map(transposeVTree).filter(x => x !== null),
         (...children) => ({
           sel: vTree.sel,
           data: vTree.data,
