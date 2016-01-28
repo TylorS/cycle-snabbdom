73e89f81512b68720345048591a8c270b524b6d9
TylorS <tlsteinberger167@gmail.com>
2016-01-28 06:29:49 -0500

    fix(transposition): handle cases where only child is null
    Adds fixes where the only child of a vNode is `null`
    
    Closes #9

--- src/transposition.js
+++ src/transposition.js
@@ -1,27 +1,37 @@
 import Rx from 'rx'
 
+function createVTree(vTree, children) {
+  return {
+    sel: vTree.sel,
+    data: vTree.data,
+    text: vTree.text,
+    elm: vTree.elm,
+    key: vTree.key,
+    children,
+  }
+}
+
 function transposeVTree(vTree) {
   if (!vTree) {
     return null
-  } else if (typeof vTree.data === `object` && vTree.data.static) {
+  } else if (vTree && typeof vTree.data === `object` && vTree.data.static) {
     return Rx.Observable.just(vTree)
   } else if (typeof vTree.subscribe === `function`) {
     return vTree.flatMapLatest(transposeVTree)
   } else if (typeof vTree === `object`) {
-    if (vTree.children && vTree.children.length > 0) {
-      return Rx.Observable.combineLatest(
-        vTree.children.map(transposeVTree).filter(x => x !== null),
-        (...children) => ({
-          sel: vTree.sel,
-          data: vTree.data,
-          text: vTree.text,
-          elm: vTree.elm,
-          key: vTree.key,
-          children,
-        })
-      )
+    if (!vTree.children || vTree.children.length === 0) {
+      return Rx.Observable.just(vTree)
     }
-    return Rx.Observable.just(vTree)
+
+    const vTreeChildren = vTree.children
+      .map(transposeVTree).filter(x => x !== null)
+
+    return vTreeChildren.length === 0 ?
+      Rx.Observable.just(createVTree(vTree, vTreeChildren)) :
+      Rx.Observable.combineLatest(
+        vTreeChildren,
+        (...children) => createVTree(vTree, children)
+      )
   } else {
     throw new Error(`Unhandled vTree Value`)
   }
