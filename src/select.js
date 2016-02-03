142dec5c1d9b1deefcfe84ae93dcc7d084c3299a
TylorS <tlsteinberger167@gmail.com>
2016-02-03 13:13:02 -0500

    fix(select): sort the namespace for proper select()ing of elements
    Implements a sorting function which sorts the namespace for
    proper selecting of elements.

--- src/select.js
+++ src/select.js
@@ -25,6 +25,18 @@ function makeIsStrictlyInRootScope(namespace) {
   }
 }
 
+const isValidString = param => typeof param === `string` && param.length > 0
+const startsWith = (string, start) => string[0] === start
+const isSelector = param =>
+    isValidString(param) && startsWith(param, `.`) || startsWith(param, `#`)
+
+function sortNamespace(a, b) {
+  if (isSelector(a) && isSelector(b)) {
+    return 0
+  }
+  return isSelector(a) ? 1 : -1
+}
+
 function makeElementSelector(rootElement$) {
   return function elementSelector(selector) {
     if (typeof selector !== `string`) {
@@ -36,7 +48,7 @@ function makeElementSelector(rootElement$) {
     const trimmedSelector = selector.trim()
     const childNamespace = trimmedSelector === `:root` ?
       namespace :
-      namespace.concat(trimmedSelector)
+      namespace.concat(trimmedSelector).sort(sortNamespace)
     const element$ = rootElement$.map(rootEl => {
       if (childNamespace.join(``) === ``) {
         return rootEl
